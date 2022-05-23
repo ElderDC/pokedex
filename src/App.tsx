e4dcfcbe2672ced668c45b5e2e7c649235ffd9df
ElderDC <eldergabrieldcr@gmail.com>
2022-05-23 12:33:58 -0400

    linter and formatter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 function App() {
-  return (
-    <div className="App">
-      <button className="btn">Hello daisyUI</button>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<button className='btn'>Hello daisyUI</button>
+		</div>
+	);
 }
 
 export default App;
