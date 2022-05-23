6c1484dd95c9242dc5eda472f3c2ddb6cc467037
ElderDC <eldergabrieldcr@gmail.com>
2022-05-23 14:45:36 -0400

    semicolons set to false

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
+// const RULES = {
+// 	OFF: 'off',
+// 	ERROR: 'error',
+// 	WARN: 'warn',
+// }
+
 module.exports = {
 	env: {
 		browser: true,
@@ -24,4 +30,4 @@ module.exports = {
 	},
 	plugins: ['react', '@typescript-eslint'],
 	rules: {},
-};
+}
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 		<div className='App'>
 			<button className='btn'>Hello daisyUI</button>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
