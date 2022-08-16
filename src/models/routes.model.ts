bea909070659a86ac53f1ab5bce18944f9c6f474
ElderDC <eldergabrieldcr@gmail.com>
2022-08-16 19:50:37 -0400

    PublicRoutes enum updated to const

--- src/models/routes.model.ts
+++ src/models/routes.model.ts
@@ -1,5 +1,5 @@
-export enum PublicRoutes {
-	'HOME' = '/home',
-	'POKEMON' = '/pokemon/:pokemon',
-	'NOT_FOUND' = '/not-found',
+export const PublicRoutes = {
+	'HOME': '/home',
+	'POKEMON': '/pokemon/:pokemon',
+	'NOT_FOUND': '/not-found',
 }
